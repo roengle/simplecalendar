d210293ea8fb7aa8c6beb9c11d5bdfb6035b4ec5
roengle <robertibengle@gmail.com>
2023-05-09 10:48:52 -0700

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:simplecalendar_raw/presentation/Entype.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -66,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState(){
-    MyAppState().getId();
+    MyAppState().getUuid();
     //Init code here
     super.initState();
   }
@@ -80,18 +81,14 @@ class _MyHomePageState extends State<MyHomePage> {
         page = HomePage();
         break;
       case 1:
-        //Favorites
-        page = MyCalendarPage();
+        //Friends
+        page = MyFriendPage();
         break;
       case 2:
-        //Friends
-        page = Placeholder();
-        break;
-      case 3:
         //Settings
         page = SettingsPage();
         break;
-      case 4:
+      case 3:
         //Info
         page = InfoPage();
         break;
@@ -110,10 +107,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     NavigationRailDestination(
                       icon: Icon(Icons.home),
                       label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Entype.calendar),
-                      label: Text('My Schedule'),
                     ),
                     NavigationRailDestination(
                         icon: Icon(Entype.shareable),
@@ -151,24 +144,330 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Meeting> source){
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
+
+class Meeting {
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
+}
+
+class _HomePageState extends State<HomePage> {
+  CalendarController _controller = CalendarController();
+  var calendarView = CalendarView.day;
+
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now();
+    DateTime startTime =
+    DateTime(today.year, today.month, today.day, 10, 0, 0);
+    DateTime endTime = startTime.add(const Duration(minutes: 75));
+    meetings.add(
+      Meeting('CS4750 Presentation', startTime, endTime, const Color(0xFF0F8644), false)
+    );
+
+    startTime =
+    DateTime(today.year, today.month, today.day, 14, 30, 0);
+    endTime = startTime.add(const Duration(hours: 2));
+    meetings.add(
+        Meeting("HRT3120 Presentation", startTime, endTime, const Color(0xFF0F8644), false)
+    );
+
+    startTime =
+        DateTime(today.year, today.month, today.day, 12, 30, 0);
+    endTime = startTime.add(const Duration(minutes: 90));
+    meetings.add(
+        Meeting("Joe - Gym", startTime, endTime, const Color.fromRGBO(255, 0, 0, 1), false)
+    );
+
+    return meetings;
+  }
+
+  void initState(){
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Center(
-      
+    return SafeArea(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: (){
+                setState(() {
+                  _controller.view = CalendarView.day;
+                });
+              }, child: Text("Day")),
+              SizedBox(width: 10),
+              ElevatedButton(onPressed: (){
+                setState(() {
+                  _controller.view = CalendarView.week;
+                });
+              }, child: Text("Week")),
+              SizedBox(width: 10),
+              ElevatedButton(onPressed: (){
+                setState(() {
+                  _controller.view = CalendarView.month;
+                });
+              }, child: Text("Month")),
+              SizedBox(width: 10),
+              ElevatedButton.icon(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (content) => NewCalendarItemPage()));
+              }, icon: Icon(Icons.add), label: Text("Add"))
+            ],
+          ),
+          Expanded(
+            child: SfCalendar(
+              view: calendarView,
+              controller: _controller,
+              monthViewSettings: MonthViewSettings(showAgenda: true),
+              dataSource: MeetingDataSource(_getDataSource()),
+            ),
+          ),
+        ]
+      ),
     );
   }
 }
 
-class MyCalendarPage extends StatelessWidget{
+class NewCalendarItemPage extends StatefulWidget{
+  @override
+  State<NewCalendarItemPage> createState() => _NewCalendarItemPageState();
+}
+
+class _NewCalendarItemPageState extends State<NewCalendarItemPage> {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    DateTime now = DateTime.now();
 
+    String startDay = DateFormat("MM/dd, yyyy").format(now);
+    String startTime = "00:00";
+    String endDay = DateFormat("MM/dd, yyyy").format(now);
+    String endTime = "00:00";
+
+    return SafeArea(
+        child: Container(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  IconButton(icon: Icon(Icons.arrow_back),
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                  ),
+                  SizedBox(width: 290,),
+                  TextButton(onPressed: (){
+
+                  }, child: Text("Add"),)
+                ],
+              ),
+              SizedBox(height: 50,),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 20,),
+                      DefaultTextStyle(
+                        style: Theme.of(context).textTheme.titleMedium!,
+                        child: Text('Name'),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    children: [
+                      SizedBox(width: 20,),
+                      DefaultTextStyle(
+                        style: Theme.of(context).textTheme.titleMedium!,
+                        child: Text('Start'),
+                      ),
+                      SizedBox(width: 140),
+                      ElevatedButton(
+                          onPressed: (){
+                            showDatePicker(context: context,
+                                initialDate: now,
+                                firstDate: DateTime(now.year, now.month, 0),
+                                lastDate: DateTime(now.year, now.month + 1, 0)
+                            );
+                            },
+                          child: Text(startDay)
+                      ),
+                      SizedBox(width: 10,),
+                      ElevatedButton(
+                        onPressed: (){
+                          showTimePicker(context: context,
+                              initialTime: TimeOfDay(hour: 0, minute: 0)
+                          );
+                        },
+                        child: Text(startTime),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(width: 20,),
+                      DefaultTextStyle(
+                        style: Theme.of(context).textTheme.titleMedium!,
+                        child: Text('End'),
+                      ),
+                      SizedBox(width: 148),
+                      ElevatedButton(
+                          onPressed: (){
+                            showDatePicker(context: context,
+                                initialDate: now,
+                                firstDate: DateTime(now.year, now.month, 0),
+                                lastDate: DateTime(now.year, now.month + 1, 0)
+                            );
+                          },
+                          child: Text(endDay)
+                      ),
+                      SizedBox(width: 10,),
+                      ElevatedButton(
+                        onPressed: (){
+                          showTimePicker(context: context,
+                              initialTime: TimeOfDay(hour: 0, minute: 0)
+                          );
+                        },
+                        child: Text(endTime),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
+
+class MyFriendPage extends StatefulWidget{
+  @override
+  State<MyFriendPage> createState() => _MyFriendPageState();
+}
+
+class _MyFriendPageState extends State<MyFriendPage> {
+  final TextEditingController _textFieldController = TextEditingController();
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Friend ID'),
+            content: TextField(
+              onChanged: (value) {
+                setState(() {
+                  valueText = value;
+                });
+              },
+              controller: _textFieldController,
+              decoration:
+              const InputDecoration(hintText: "Friend ID"),
+            ),
+            actions: <Widget>[
+              MaterialButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              MaterialButton(
+                color: Colors.green,
+                textColor: Colors.white,
+                child: const Text('OK'),
+                onPressed: () {
+                  setState(() {
+                    //Really should be checking DB and getting name for it, don't have time
+                    if(valueText! == "1"){
+                      friends.add("Joe");
+                    }
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  var friends = <String>{};
+  String? codeDialog;
+  String? valueText;
+
+  Set<String> getFriends(){
+    return friends;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Center(
-
+      child: SafeArea(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: (){
+              _displayTextInputDialog(context);
+            }, child: Text("Add")),
+            for(var friendName in friends)
+              Row(
+                children: [
+                  SizedBox(width: 20,),
+                  SizedBox(width: 200, child: Text(friendName)),
+                  ElevatedButton.icon(onPressed: (){
+                    setState(() {
+                      friends.remove(friendName);
+                    });
+                  }, icon: Icon(Icons.delete), label: Text("Delete"))
+                ],
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -258,35 +557,6 @@ class InfoPage extends StatelessWidget{
           Text("Version 0.1.0"),
           Text("Developed by Robert Engle for CS4750")
         ],
-      ),
-    );
-  }
-
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase,
-          style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",
-        ),
       ),
     );
   }
